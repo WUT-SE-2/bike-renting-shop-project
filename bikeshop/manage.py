fb26283d44e69d1aeafa62b5a7ed40c81773c6d8
HilalQassem <hilal.kasem24@gmail.com>
2023-04-02 15:16:04 +0200

    Fixed action, again! (supposedly :))

--- bikeshop/manage.py
+++ bikeshop/manage.py
@@ -2,9 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
-
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bikeshop.settings')
@@ -17,7 +14,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
