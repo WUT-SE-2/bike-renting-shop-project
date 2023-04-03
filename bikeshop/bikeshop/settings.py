40263fcb597f0142901fb4ef71a9bc92a77dfecf
Filip <filip.rak.20001@gmail.com>
2023-04-03 10:29:35 +0200

    db-local

--- bikeshop/bikeshop/settings.py
+++ bikeshop/bikeshop/settings.py
@@ -88,11 +88,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT')
+            'OPTIONS':{
+                'service': 'local_settings',
+                'passfile': '.my_pgpass',
+            },
         }
     }
 
