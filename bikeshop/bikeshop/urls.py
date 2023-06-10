e205c1ebf8d08d52cee59c1238396e7853b6cbbe
HilalQassem <116579035+HilalQassem@users.noreply.github.com>
2023-06-10 19:22:34 +0200

    fix deploy settings and migrate to db

--- bikeshop/bikeshop/urls.py
+++ bikeshop/bikeshop/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,4 @@ urlpatterns = [
     path('complaint/', include('complaint.urls')),
     path('comment/', include('comment.urls')),
 ]
+urlpatterns+=staticfiles_urlpatterns()
