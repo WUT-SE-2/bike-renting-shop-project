8513228d720de92a9fc295494d9d6f8784a90ed7
HilalQassem <116579035+HilalQassem@users.noreply.github.com>
2023-06-12 12:59:07 +0200

    Update settings.py

--- bikeshop/bikeshop/settings.py
+++ bikeshop/bikeshop/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL='static/'
+MEDIA_URL='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -167,4 +167,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     'static/',
+    'media/',
 ]
